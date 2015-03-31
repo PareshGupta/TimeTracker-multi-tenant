97385193309d7ca6f5097a7ace35a963fce8173a
PareshGupta <paresh@vinsol.com>
2015-03-31 20:54:09 +0530

    add user controller + inviting user functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,13 +9,17 @@ class ApplicationController < ActionController::Base
       Apartment::Tenant.switch!('public')
       return unless request.subdomain.present?
 
-      account = Account.find_by(subdomain: request.subdomain)
-      if account      
+      if current_account
         Apartment::Tenant.switch!(request.subdomain)
       else
         redirect_to root_url(subdomain: false)
       end
     end
+
+    def current_account
+      @current_account ||= account = Account.find_by(subdomain: request.subdomain)
+    end
+    helper_method :current_account
 
     def after_sign_out_path_for(resource)
       new_user_session_path
